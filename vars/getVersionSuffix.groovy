3e57b9245b2a0985d37ccbbacc8001916fe6b7bf
bogdanagache <bmaga2003@yahoo.co.uk>
2020-12-12 07:15:47 +0200

    fix issues

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -2,7 +2,8 @@ def call(Map config) {
     node {
         if (config.isReleaseCandidate) {
             return config.rcNumber
-        } else {
+        }
+        {
             return config.rcNumber + '+ci.' + env.BUILD_NUMBER
         }
     }
